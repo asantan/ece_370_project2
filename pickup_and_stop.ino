ef01a46bf7990025d5b0f2549b6269b2b5c91933
asantan <adalsantana.c@gmail.com>
2019-04-29 23:16:13 -0400

    Add files via upload
    loop function populated and minor bug fixes

--- pickup_and_stop.ino
+++ pickup_and_stop.ino
@@ -1,4 +1,4 @@
-include <LSM303.h>
+#include <LSM303.h>
 #include <Wire.h>
 
 #define MOTOR1_PINA 10
@@ -13,46 +13,41 @@ double oldimu_y;
 
 bool pickedup = false; 
 
-void setup(){
+void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
   Wire.begin();
   imu.init();
   imu.enableDefault();
   imu.read();
-  oldimu_y = (double) imu.a.y*0.061/1000.0; 
-  pinMode(MOTOR1_PINA, OUTPUT); 
-  pinMode(MOTOR1_PINB, OUTPUT);
-  pinMode(MOTOR2_PINA, OUTPUT); 
-  pinMode(MOTOR2_PINB, OUTPUT); 
-  analogWrite(MOTOR1_PINA, 0);
-  analogWrite(MOTOR1_PINA, 0);
-  analogWrite(MOTOR2_PINB, 0);
+  oldimu_y = (double)imu.a.y*0.061/1000.0;
 }
 
 int setSpeed(float s){
-  f(s > 1.0) s = 1.0; 
+  if(s > 1.0) s = 1.0; 
   if(s < 0.0) s = 0.0; 
   int out = (int) (s*255.0); 
   return out; 
 }
 
+
 void setVelocity(float velocity){
-float s = velocity; 
+  float s = velocity; 
   if(s < 0) s = -s; 
   int sp1 = setSpeed(s);  //speed motor 1
   int sp2 = setSpeed(s);                //speed motor 2
-  if(!motors.pickedup){
+  if(!pickedup){
     if(velocity >= 0){ //forward  
-      analogWrite(MOTOR1_PINA, 0);
+      analogWrite(MOTOR1_PINB, 0);
       analogWrite(MOTOR1_PINA, sp1);
-      analogWrite(MOTOR2_PINB, 0);
+      analogWrite(MOTOR2_PINA, 0);
       analogWrite(MOTOR2_PINB, sp2);
     }
     else { //backward
       analogWrite(MOTOR1_PINA, 0);
-      analogWrite(MOTOR1_PINA, sp1);
+      analogWrite(MOTOR1_PINB, sp1);
       analogWrite(MOTOR2_PINB, 0);
-      analogWrite(MOTOR2_PINB, sp2);
+      analogWrite(MOTOR2_PINA, sp2);
     }
   }
   else{
@@ -63,6 +58,7 @@ float s = velocity;
   }
 }
 
+
 void checkIMU(){
   imu.read();
   double changeIn = (double)imu.a.y*0.061/1000.0;
@@ -71,6 +67,13 @@ void checkIMU(){
     Serial.println("Pickup detected");
     pickedup = true; 
   }
+ 
+  oldimu_y = (double)imu.a.y*0.061/1000.0;
 }
 
-void loop(){}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  checkIMU();
+  setVelocity(0.5); 
+}
