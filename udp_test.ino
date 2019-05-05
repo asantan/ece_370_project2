888529f5b45ee0ff018c1068c11a8eb5e95e27ee
asantan <adalsantana.c@gmail.com>
2019-05-05 01:53:37 -0400

    Add files via upload
    access point mode set up

--- udp_test.ino
+++ udp_test.ino
@@ -1,5 +1,3 @@
-#include <LSM303.h>
-#include <Wire.h>
 #include <WiFi101.h>
 #include "secrets.h"
 
@@ -12,10 +10,12 @@ char ssid[] = SECRET_SSID;  // network SSID)
 char pass[] = SECRET_PASS;  //network password
 
 int status = WL_IDLE_STATUS; 
-WiFiServer(80); 
+WiFiServer server(80); 
 
 void setup() {
-
+  Serial.begin(9600);
+  motorsetup();
+  APsetup(); 
 }
 
 void APsetup(){
@@ -26,21 +26,25 @@ void APsetup(){
     while(true);
   }
 
-  //connect to WiFi network
-  while(status != WL_CONNECTED){
-    Serial.println("Attempting to connect to SSID: ");
-    Serial.println(SECRET_SSID);
-    status = WiFi.begin(ssid, pass);
-    //wait 10 seconds
-    delay(10000); 
+  // by default the local IP address of will be 192.168.1.1
+  // you can override it with the following:
+  WiFi.config(IPAddress(10, 0, 0, 30));
+  
+  // print the network name (SSID);
+  Serial.print("Creating access point named: ");
+  Serial.println(ssid);
+
+  status = WiFi.beginAP(ssid);
+  if (status != WL_AP_LISTENING) {
+    Serial.println("Creating access point failed");
+    // don't continue
+    while (true);
   }
-
-  Serial.println("Connected to wifi");
-  printWiFiStatus();
-
-  Serial.println("\nStarting connection to server");
-  //if connection received report via serial
-  Udp.begin(UDP_PORT);
+  //wait 10 seconds for connection
+  delay(10000);
+  //start web server on port 80
+  server.begin();
+  printWiFiStatus(); 
 }
 
 void motorsetup(){
@@ -54,42 +58,98 @@ void motorsetup(){
   analogWrite(MOTOR2_PINB, 0);
 }
 
-int setSpeed(float s){
-  if(s > 1.0) s = 1.0; 
-  if(s < 0.0) s = 0.0; 
-  int out = (int) (s*255.0); 
-  return out; 
-}
-
-
-void setVelocity(float velocity){
-  float s = velocity; 
-  if(s < 0) s = -s; 
-  int sp1 = setSpeed(s);  //speed motor 1
-  int sp2 = setSpeed(s);                //speed motor 2
-  if(!pickedup){
-    if(velocity >= 0){ //forward  
-      analogWrite(MOTOR1_PINB, 0);
-      analogWrite(MOTOR1_PINA, sp1);
-      analogWrite(MOTOR2_PINA, 0);
-      analogWrite(MOTOR2_PINB, sp2);
-    }
-    else { //backward
-      analogWrite(MOTOR1_PINA, 0);
-      analogWrite(MOTOR1_PINB, sp1);
-      analogWrite(MOTOR2_PINB, 0);
-      analogWrite(MOTOR2_PINA, sp2);
-    }
-  }
-  else{
-      analogWrite(MOTOR1_PINA, 0);
-      analogWrite(MOTOR1_PINB, 0);
-      analogWrite(MOTOR2_PINB, 0);
-      analogWrite(MOTOR2_PINA, 0);
-  }
-}
-
 void loop() {
-  // put your main code here, to run repeatedly:
+  // compare the previous status to the current status
+  if (status != WiFi.status()) {
+    // it has changed update the variable
+    status = WiFi.status();
+
+    if (status == WL_AP_CONNECTED) {
+      byte remoteMac[6];
+
+      // a device has connected to the AP
+      Serial.print("Device connected to AP, MAC address: ");
+      WiFi.APClientMacAddress(remoteMac);
+      Serial.print(remoteMac[5], HEX);
+      Serial.print(":");
+      Serial.print(remoteMac[4], HEX);
+      Serial.print(":");
+      Serial.print(remoteMac[3], HEX);
+      Serial.print(":");
+      Serial.print(remoteMac[2], HEX);
+      Serial.print(":");
+      Serial.print(remoteMac[1], HEX);
+      Serial.print(":");
+      Serial.println(remoteMac[0], HEX);
+    } else {
+      // a device has disconnected from the AP, and we are back in listening mode
+      Serial.println("Device disconnected from AP");
+    }
+  }
+  
+  WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {                             // if you get a client,
+    Serial.println("new client");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/H\">here</a> turn the LED on<br>");
+            client.print("Click <a href=\"/L\">here</a> turn the LED off<br>");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          }
+          else {      // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        }
+        else if (c != '\r') {    // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
+
+
+void printWiFiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+  // print where to go in a browser:
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
 
 }
